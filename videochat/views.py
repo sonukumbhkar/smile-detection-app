d02074e6285f05d629054f1fd2e6689556b8cc86
Sonu kumbhkar <59777243+sonukumbhkar@users.noreply.github.com>
2021-05-04 11:20:12 +0530

    10

--- videochat/views.py
+++ videochat/views.py
@@ -12,14 +12,15 @@ def index(request):
     
 
 def get_frame():
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     while True:
         ret,img = camera.read()
         img= cv2.flip(img, 1)
         imencdode = cv2.imencode('.jpeg', img)[1]
         stringData = imencdode.tostring()
         yield(b'--frame\r\n'b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
-    del(camera)
+    camera.release()
+    cv2.destroyAllWindows()
 def dynamic_stream(request,stream_path='video'):
     try:
         return StreamingHttpResponse(get_frame(),content_type='multipart/x-mixed-replace;boundary=frame')
