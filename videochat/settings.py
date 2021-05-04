4b57778fae4070f9ffe0932820f75246973d2f0e
Sonu kumbhkar <59777243+sonukumbhkar@users.noreply.github.com>
2021-05-04 09:00:35 +0530

    3

--- videochat/settings.py
+++ videochat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jnd-i6n_g)ut^$e5@4=*!velyec(jipk(n5vdcgop+#1h@!0zm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['videochitchat.herokuapp.com','127.0.0.1']
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'videochat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT  =   BASE_DIR / 'static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
